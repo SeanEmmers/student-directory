04e18552c52960578a5f159e5eb52f578d542184
Sean Phillips <seanphillips@Seans-Air.broadband>
2021-05-17 11:29:24 +0100

    Adding methods to the directory

--- directory.rb
+++ directory.rb
@@ -12,11 +12,23 @@ students = [
     "Joffrey Baratheon",
     "Norman Bates"
 ]
-# Printing out the students
-puts "The students of Villains Academy"
-puts "-------------"
-students.each do |student|
-    puts student
-end
-# Printing the total number of students
-puts "Overall, we have #{students.count} great students"
+
+def print_header
+    puts "The students of Villains Academy"
+    puts "-------------"
+  end
+  
+  def print(names)
+    names.each do |name|
+      puts name
+    end
+  end
+  
+  def print_footer(names)
+    puts "Overall, we have #{names.count} great students"
+  end
+
+#Calling the methods
+print_header
+print(students)
+print_footer(students)
